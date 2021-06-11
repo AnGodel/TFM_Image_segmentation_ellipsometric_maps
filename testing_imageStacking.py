006883368541372ec24fa298b834bb65ea211aab
AnGodel <amgonzalezdelgado@gmail.com>
2021-06-11 18:36:37 +0200

    added nanofilm package installer
    re-tested KMeans clustering with stacks. Score still unreliable
    -Had to install yellowbrick library using pip -U in a new conda environment, as it was unusable otherwise, throwing errors related to some modules of sklearn.utils not being loadable

--- testing_imageStacking.py
+++ testing_imageStacking.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 from astropy.convolution import Gaussian2DKernel, interpolate_replace_nans
 from sklearn.cluster import KMeans
 import MyPacket.mypacket as mp
-from sklearn.cluster import KMeans
+from yellowbrick.cluster import KElbowVisualizer
+
+#setting the paths to demo delta and psi maps
 
 delta1 = './data_demo/demofiles_flake/20150513_IMAGE_autosave_00018.png'
 delta2 = './data_demo/demofiles_flake/20150513_IMAGE_autosave_00020.png'
@@ -22,17 +24,36 @@ psi1 = './data_demo/demofiles_flake/20150513_IMAGE_autosave_00019.png'
 psi2 = './data_demo/demofiles_flake/20150513_IMAGE_autosave_00021.png'
 psi3 = './data_demo/demofiles_flake/20150513_IMAGE_autosave_00023.png'
 
-
+#transforming the raw maps into two lists of processed 'images'
+#preprocessing includes bit transformation into single-channel images with nanofilm.imread 
+#and smoothing/NaN removal using convolution kernel with astropy 
 
 deltas = list(map(mp.loadmap_astroclean, [delta1, delta2, delta3]))
 psis = list(map(mp.loadmap_astroclean, [psi1, psi2, psi3]))
 
+#the images are stacked by type using np.dstack
+
 deltaStack = np.dstack(deltas)
 psiStack = np.dstack(psis)
 
+#then they are reshaped and KMeans segmentation is applied. 
+
 DStackReshaped = deltaStack.reshape(deltaStack.shape[0]*deltaStack.shape[1], deltaStack.shape[2])
 
-kmeans = KMeans(n_clusters=6, random_state=0).fit(DStackReshaped)
+#the yellowbrick library is used for the evaluation of the KMeans clustering
+#the results of this evaluation will give an indication of the right number of clusters,
+#but this results should not be taken literally 
+
+model = KMeans(random_state=0)
+visualizer = KElbowVisualizer(model, k=(2,13))
+
+visualizer.fit(DStackReshaped)
+visualizer.show()
+
+#After segmentation, they are plotted
+
+
+kmeans = KMeans(n_clusters=5, random_state=0).fit(DStackReshaped)
 
 segmented = kmeans.cluster_centers_[kmeans.labels_]
 
@@ -42,7 +63,7 @@ mp.plot_image_withCbar(np.dsplit(segmentedStack, deltaStack.shape[2])[0], 'delta
 mp.plot_image_withCbar(np.dsplit(segmentedStack, deltaStack.shape[2])[1], 'delta2Cluster')
 mp.plot_image_withCbar(np.dsplit(segmentedStack, deltaStack.shape[2])[2], 'delta3Cluster')
 
-
+#same procedure for psi maps
 
 PStackReshaped = psiStack.reshape(psiStack.shape[0]*psiStack.shape[1], psiStack.shape[2])
 
@@ -55,3 +76,5 @@ segmentedStack = segmented.reshape(psiStack.shape[0], psiStack.shape[1], psiStac
 mp.plot_image_withCbar(np.dsplit(segmentedStack, psiStack.shape[2])[0], 'psi1Cluster')
 mp.plot_image_withCbar(np.dsplit(segmentedStack, psiStack.shape[2])[1], 'psi2Cluster')
 mp.plot_image_withCbar(np.dsplit(segmentedStack, psiStack.shape[2])[2], 'psi3Cluster')
+
+
