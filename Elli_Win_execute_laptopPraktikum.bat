e0f1b26fba33eab2a2b9bd01be4a5e7414d22520
Antonio González <74109963+AnGodel@users.noreply.github.com>
2021-07-07 15:29:36 +0200

    Update Elli_Win_execute_laptopPraktikum.bat

--- Elli_Win_execute_laptopPraktikum.bat
+++ Elli_Win_execute_laptopPraktikum.bat
@@ -1,6 +1,6 @@
 
 cd C:\Users\ago\Anaconda3\Scripts
 conda init cmd.exe
-CALL conda.bat activate project_ago
+CALL conda.bat activate tfm
 
 streamlit run "C:\Users\ago\Documents\mygitprojects\TFM_Image_segmentation_ellipsometric_maps\ElliMaps_segmentation.py"
